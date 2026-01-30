e9f63a68e4549284148876f6f4ee494e93526774
virendra vyas <vvyas@systango.com>
2026-01-30 13:28:10 +0530

    test leak on ci

--- logging/log.go
+++ logging/log.go
@@ -14,6 +14,12 @@ func init() {
 		Level(zerolog.InfoLevel).
 		With().Timestamp().Logger()
 }
+const a=AKIAFAKE1234567890AB
+const s=wJalrXUtnFEMI/K7MDENG/bPxRfiCYFAKEKEY
+const e='0d7f0363db643cea4c685e56672351d804e7c39b3367c7cffd77176270836691';
+const gcp=AIzaSyZzXxYyWwVvUuTtSsRrQqPpOoNnMmLlKk9
+const azuresec=abc8Q~aBcDeFgHiJkLmNoPqRsTuVwXyZ12345
+const azid=3f8c9e41-2d6b-4f0a-9b5d-91c8f2e9a7b4
 
 func With() zerolog.Context {
 	return Logger.With()
